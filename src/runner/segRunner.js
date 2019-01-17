a1a7df906191e9d55ef17959dffe87b1030cfbae
wc5858 <wc47501@gmail.com>
2019-01-17 13:18:15 +0800

    add：getLCSS
    对getComputedStyle做去冗余操作

--- src/runner/segRunner.js
+++ src/runner/segRunner.js
@@ -3,25 +3,28 @@ const seg = require('../segmentation/index');
 const saveImg = require('../util/saveImage');
 
 const list = [
-    //'www.google.com',
+    'www.google.com',
     //'www.bilibili.com',
     //'github.com',
     //'stackoverflow.com',
-    'news.ycombinator.com'
+    // 'news.ycombinator.com'
 ]
 
 module.exports = async function () {
     let driver = await new Builder().forBrowser('chrome').build();
     try {
         for (let site of list) {
-            for (let i = 1; i <= 10; i++) {
+            let i =5
+            // for (let i = 1; i <= 10; i++) {
                 await driver.get('https://' + site);
                 let data = await seg(driver, {
-                    pac: i
+                    pac: i,
+                    returnType : 'wprima'
                 })
+                console.log(data)
                 let imgData = await driver.takeScreenshot()
                 saveImg(site + 'pac-' + i, imgData)
-            }
+            // }
         }
 
     } catch (e) {
