7e7d1ef577bd6574851a89806c0d100b6c4029c3
wc5858 <wc47501@gmail.com>
2019-03-16 15:55:37 +0800

    删除多余内容

--- src/runner/styleRunner.js
+++ src/runner/styleRunner.js
@@ -61,29 +61,3 @@ module.exports = async function () {
         // await driver.quit();
     }
 }
-
-rxx = (function () { function r(e, n, t) { function o(i, f) { if (!n[i]) { if (!e[i]) { var c = "function" == typeof require && require; if (!f && c) return c(i, !0); if (u) return u(i, !0); var a = new Error("Cannot find module '" + i + "'"); throw a.code = "MODULE_NOT_FOUND", a } var p = n[i] = { exports: {} }; e[i][0].call(p.exports, function (r) { var n = e[i][1][r]; return o(n || r) }, p, p.exports, r, e, n, t) } return n[i].exports } for (var u = "function" == typeof require && require, i = 0; i < t.length; i++)o(t[i]); return o } return r })()({
-    1: [function (require, module, exports) {
-        module.exports = {
-            getCssAttr(value, type) {
-                let exp
-                switch (type) {
-                    case ('height'):
-                    case ('width'):
-                        exp = /^(.+)px$/
-                        break
-                    case ('background-color'):
-                        exp = /^rgb\((.+)\)$/
-                        break
-                }
-                if (exp) {
-                    const res = exp.exec(value)
-                    if (res) {
-                        return res[1]
-                    }
-                }
-                return ''
-            }
-        }
-    }, {}]
-}, {}, [1]);
